3e120961786eb0660009dc0bdf990d321ec0d54b
aterenin <aterenin@users.noreply.github.com>
2019-10-30 14:53:38 +0000

    Added dark mode script injection

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, systemPreferences, nativeTheme } = require('electron')
+const fs = require('fs')
+const path = require('path')
+const http = require('http')
 
-let win
+let mainWindow
+let contents
+let darkReaderJs
+let darkCSS
+let darkCSSKey
 
 function createWindow () {
-  win = new BrowserWindow({width: 1024, height: 768})
-  win.loadURL('https://photos.google.com/')
-  win.on('closed', () => {
-    win = null
+  mainWindow = new BrowserWindow({width: 1024, height: 768})
+  mainWindow.loadURL('https://photos.google.com/')
+  mainWindow.on('closed', () => {
+    mainWindow = null
   })
+  contents = mainWindow.webContents
+
+  if (process.platform === 'darwin') {
+    darkCSS = fs.readFileSync(path.join(__dirname, 'dark.css'), 'utf8')
+    darkReaderJs = fs.readFileSync(path.join(__dirname, 'darkreader.js'), 'utf8')
+    contents.on('dom-ready', () => {
+      contents.executeJavaScript(darkReaderJs)
+      updateTheme()
+    });
+    systemPreferences.subscribeNotification('AppleInterfaceThemeChangedNotification', updateTheme)
+  }
+}
+
+function updateTheme() {
+  if(nativeTheme.shouldUseDarkColors) {
+    contents.executeJavaScript(`
+      DarkReader.enable({
+        brightness: 100,
+        contrast: 90,
+        sepia: 10
+    });`)
+    contents.insertCSS(darkCSS).then(value => { darkCSSKey = value })
+  } else if(darkCSSKey !== null) {
+    contents.executeJavaScript(`
+      DarkReader.disable();
+    `)
+    contents.removeInsertedCSS(darkCSSKey).then(() => { darkCSSKey = null })
+  }
 }
 
 app.on('ready', createWindow)
@@ -19,7 +54,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow()
   }
 })
